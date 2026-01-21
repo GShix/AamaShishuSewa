55b4c9996a0885924c00f8c0f18e35dd13de98fc
Dambar Sing <programmer.gshi18@gmail.com>
2026-01-21 11:26:43 +0545

    logo updated

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-a87ad30a'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.j79d1bt7s0g"
+    "revision": "0.pnhpslkr4f8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
